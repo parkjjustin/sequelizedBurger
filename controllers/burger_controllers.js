5491ff41aab43c5b4339b21146345162e5e87efa
Justin Park <parkjjustin@gmail.com>
2017-02-17 02:49:15 -0800

    gitignore ignores node_modules

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -6,7 +6,11 @@ module.exports = (app) => {
     });
 
     app.get("/burger", (request, response) => {
-        db.burger.findAll({order: [["id"]]}).then(function(dbBurger){
+        db.burger.findAll({
+            order: [
+                ["id"]
+            ]
+        }).then(function(dbBurger) {
             var hbsObject = {
                 burger: dbBurger
             };
@@ -17,7 +21,7 @@ module.exports = (app) => {
     app.post("/burger/create", (request, response) => {
         db.burger.create({
             burger_type: request.body.burger_type
-        }).then(function(dbBurger){
+        }).then(function(dbBurger) {
             response.redirect("/burger");
         });
     });
@@ -25,18 +29,11 @@ module.exports = (app) => {
     app.put("/burger/update/:id", function(request, response) {
         db.burger.update({
             devoured: request.body.devoured
-        },{
-            where: {id: request.params.id}
-        }).then(function(dbBurger){
+        }, {
+            where: { id: request.params.id }
+        }).then(function(dbBurger) {
             response.redirect("/burger");
         });
     });
 
-    // app.delete("/burger/delete/:id", function(request, response) {
-    //     db.burger.destroy({
-    //         where: {id: request.params.id}
-    //     }).then(function(dbBurger) {
-    //         response.redirect("/");
-    //     });
-    // });
 };
